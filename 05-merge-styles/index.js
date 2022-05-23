174b642bbde9a291964f606d048fe4af114a2cf0
AlexShumsky <sanya.shumskiy.95@mail.ru>
2022-05-24 01:24:39 +0300

    feat: add merge styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+let arr = [];
+
+const filePath = path.join(__dirname, "styles");
+const filePathTo = path.join(__dirname, "project-dist", "bundle.css");
+
+fs.readFile(filePathTo, (err, file) => {
+  if (err) {
+  }
+  if (file) fs.unlink(filePathTo, () => {});
+});
+
+fs.readdir(filePath, function (err, files) {
+  if (err) console.error();
+
+  for (const file of files) {
+    const stylesFile = path.resolve(filePath, file);
+    fs.stat(stylesFile, (err, stats) => {
+      if (err) console.error();
+      if (stats.isFile() && path.extname(file) == ".css") {
+        fs.readFile(stylesFile, "utf8", function (err, data) {
+          if (err) console.error();
+          arr.push(data);
+          fs.appendFile(filePathTo, arr.join("\n"), function () {});
+        });
+      }
+    });
+  }
+});
