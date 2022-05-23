a4fc3b7a821aa8d388d42283b49b54cd3ea1bba8
AlexShumsky <sanya.shumskiy.95@mail.ru>
2022-05-24 00:46:05 +0300

    feat: add copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require("fs");
+const path = require("path");
 
+const sourceFolderPath = path.join(__dirname, "files");
+const copyFolderPath = path.join(__dirname, "files-copy");
+
+fs.mkdir(copyFolderPath, { recursive: true }, (err) => {
+  if (err) console.error(err);
+  fs.readdir(sourceFolderPath, (err, files) => {
+    if (err) console.error(err);
+    for (const file of files) {
+      const filePath = path.resolve(sourceFolderPath, file);
+      const copiedFilePath = path.resolve(copyFolderPath, file);
+      fs.copyFile(
+        filePath,
+        copiedFilePath,
+        fs.constants.COPYFILE_FICLONE,
+        (err) => {
+          if (err) {
+          }
+        }
+      );
+    }
+  });
+});
