3cee8a14c5054858540c1136157facc07bfd5809
AlexShumsky <sanya.shumskiy.95@mail.ru>
2022-05-23 22:51:30 +0300

    feat: add files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
+
+const folderPath = path.join(__dirname, "secret-folder");
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) console.error("folder Error");
+  for (const file of files) {
+    const filePath = path.resolve(folderPath, file);
+    const fileExt = path.extname(file);
+    const fileName = path.basename(file, fileExt);
+
+    fs.stat(filePath, (err, stats) => {
+      if (err) console.error("stats Error");
+      if (stats.isFile()) {
+        const fileSize = (stats.size / 1024).toFixed(3);
+        process.stdout.write(`${fileName} — ${fileExt} — ${fileSize}kb\n`);
+      }
+    });
+  }
+});
